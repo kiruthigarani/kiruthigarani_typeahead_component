81cbbeb74c972c88dbd28f563e65b46b51da46ee
kiruthigaranik <kiruthigaranik@gmail.com>
2025-12-25 20:22:48 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TypeAhead from "./components/TypeAhead";
+
+export function App(props) {
+  return (
+    <div className='App'>
+      <h1>Start searching.</h1>
+      <TypeAhead />
+    </div>
+  );
+}
